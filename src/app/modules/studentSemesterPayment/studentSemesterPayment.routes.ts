3e2f9a1c3a7315028cb37e33e53fc5ea4dd1ae8e
fahimahammed <fahimahammed.cse@gmail.com>
2023-09-15 17:00:25 +0600

    student payment for each semesters

--- src/app/modules/studentSemesterPayment/studentSemesterPayment.routes.ts
+++ src/app/modules/studentSemesterPayment/studentSemesterPayment.routes.ts
@@ -1,26 +1,8 @@
 import express from 'express';
-import { ENUM_USER_ROLE } from '../../../enums/user';
-import validateRequest from '../../middlewares/validateRequest';
-import auth from '../../middlewares/auth';
-import { StudentSemesterPaymentValidation } from './studentSemesterPayment.validation';
 import { StudentSemesterPaymentController } from './studentSemesterPayment.controller';
 
 const router = express.Router();
 
 router.get('/my-semester-payments', StudentSemesterPaymentController.getMyPayment);
-
-router.post(
-  '/initiate-payment',
-  validateRequest(StudentSemesterPaymentValidation.initiatePayment),
-  auth(ENUM_USER_ROLE.STUDENT),
-  StudentSemesterPaymentController.initiatePayment
-);
-
-router.post(
-  '/complete-payment',
-  validateRequest(StudentSemesterPaymentValidation.completePayment),
-  // auth(ENUM_USER_ROLE.STUDENT),
-  StudentSemesterPaymentController.completePayment
-);
 
 export const studentSemesterPaymentRoutes = router;
