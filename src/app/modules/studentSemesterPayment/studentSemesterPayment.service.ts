edefa3013f8e3e232b4f6be78806d8bac65bc0a0
fahimahammed <fahimahammed.cse@gmail.com>
2023-09-15 16:44:43 +0600

    get my semester payment

--- src/app/modules/studentSemesterPayment/studentSemesterPayment.service.ts
+++ src/app/modules/studentSemesterPayment/studentSemesterPayment.service.ts
@@ -1,10 +1,10 @@
 import { Request } from 'express';
 import { IGenericResponse } from '../../../interfaces/common';
-import { CoreHttpService } from '../../../shared/axios';
+import { CoreService } from '../../../shared/axios';
 
 const getMyPayment = async (req: Request): Promise<IGenericResponse> => {
-  const response: IGenericResponse = await CoreHttpService.get(
-    '/student-semester-payments/my-semester-payments',
+  const response: IGenericResponse = await CoreService.get(
+    '/student-semester-payments',
     {
       params: req.query,
       headers: {
@@ -15,34 +15,6 @@ const getMyPayment = async (req: Request): Promise<IGenericResponse> => {
   return response;
 };
 
-const initiatePayment = async (req: Request): Promise<IGenericResponse> => {
-  const response: IGenericResponse = await CoreHttpService.post(
-    `/student-semester-payments/initiate-payment`,
-    req.body,
-    {
-      headers: {
-        Authorization: req.headers.authorization
-      }
-    }
-  );
-  return response;
-};
-
-const completePayment = async (req: Request): Promise<IGenericResponse> => {
-  const response: IGenericResponse = await CoreHttpService.post(
-    `/student-semester-payments/complete-payment`,
-    req.body,
-    {
-      headers: {
-        Authorization: req.headers.authorization
-      }
-    }
-  );
-  return response;
-};
-
 export const StudentSemesterPaymentService = {
-  getMyPayment,
-  initiatePayment,
-  completePayment
+  getMyPayment
 };
